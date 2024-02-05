59824852cb870a94c012a8712a5e2342211508ea
Ernest-Geek <ernestampene1@gmail.com>
2024-02-05 15:25:41 +0000

    new feature added

--- static/js/login.js
+++ static/js/login.js
@@ -1,34 +1,36 @@
-const signUpButton = document.getElementById('signUp');
-const logInButton = document.getElementById('logIn');
 const container = document.getElementById('container');
 
-signUpButton.addEventListener('click', () => {
-    container.classList.add("right-panel-active");
-});
+// Function to toggle password visibility
+function togglePassword(passwordIds, showPasswordId) {
+    const showPasswordCheckbox = document.getElementById(showPasswordId);
 
-logInButton.addEventListener('click', () => {
-    container.classList.remove("right-panel-active");
-});
-
-// Password toggle functionality
-function togglePassword(passwordId, showPasswordId) {
-    var passwordInput = document.getElementById(passwordId);
-    var showPasswordCheckbox = document.getElementById(showPasswordId);
-
-    if (showPasswordCheckbox.checked) {
-        passwordInput.type = "text";
-    } else {
-        passwordInput.type = "password";
-    }
+    passwordIds.forEach((passwordId) => {
+        const passwordInput = document.getElementById(passwordId);
+        passwordInput.type = showPasswordCheckbox.checked ? "text" : "password";
+    });
 }
+
+// Function to handle the sign-up and log-in buttons
+function handleButtonClick(isSignUp) {
+    container.classList.toggle("right-panel-active", isSignUp);
+}
+
+// Event listeners for sign-up and log-in buttons
+document.getElementById('signUp').addEventListener('click', () => {
+    handleButtonClick(true);
+});
+
+document.getElementById('logIn').addEventListener('click', () => {
+    handleButtonClick(false);
+});
 
 // Event listener for the showPassword checkbox in the sign-up form
 document.getElementById("showPasswordSignUp").addEventListener("change", function() {
-    togglePassword("passwordSignUp", "showPasswordSignUp");
+    togglePassword(["passwordSignUp", "confirmPasswordSignUp"], "showPasswordSignUp");
 });
 
 // Event listener for the showPassword checkbox in the log-in form
 document.getElementById("showPasswordLogIn").addEventListener("change", function() {
-    togglePassword("passwordLogIn", "showPasswordLogIn");
+    togglePassword(["passwordLogIn"], "showPasswordLogIn");
 });
 
